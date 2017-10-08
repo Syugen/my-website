411cfd469416eb1c3a83d7733c7902d330a17803
Syugen <syugen.zhou@gmail.com>
2017-10-07 21:11:43 -0400

    Some projects added

--- mySite.js
+++ mySite.js
@@ -6,10 +6,6 @@ var bodyParser = require("body-parser");
 var http = require("http");
 var server = http.createServer(app);
 
-//var video = require("./routes/video");
-//var user = require("./routes/user");
-//var api = require("./routes/api");
-
 app.use(express.static(__dirname + '/assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,12 +20,22 @@ app.get("/project", function(req, res) {
     res.sendFile(__dirname + "/public/project.html");
 });
 
+app.get("/project/:name", function(req, res) {
+    var path = __dirname + "/public/projects/" + req.params.name + ".html";
+    var fs = require('fs');
+    if (fs.existsSync(path)) {
+        res.sendFile(path);
+    } else {
+        res.status(404).send("DNE (For a better 404 page, constructing)");
+    }
+});
+
 app.get("/interest", function(req, res) {
     res.send("Constructing");
 });
 
 app.get("*", function(req, res) {
-    res.status(404).send("404");
+    res.status(404).send("404 (For a better 404 page, constructing)");
 });
 
 server.listen(3003, function(request, response) {
